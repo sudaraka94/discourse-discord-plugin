32080878c291cfde27f299b5d72cf4e1472d0d48
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2019-04-13 00:26:34 +0800

    Add add_user_role and remove_user_role wrappers

--- lib/discourse_discord.rb
+++ lib/discourse_discord.rb
@@ -1,9 +1,33 @@
 class DiscourseDiscord
-  def self.add_role(email, role)
+  def self.add_role(guild_id, user_id, role_id)
+    discord_host = SiteSetting.discord_host
+    response = Faraday.put do |req|
+      req.url "#{discord_host}/guilds/#{guild_id}/members/#{user_id}/roles/#{role_id}"
+    end
 
+    begin
+      parsed_response=JSON.parse response.body
+      if parsed_response["status"] == "error"
+        Rails.logger.error "DISCOURSE DISCORD PLUGIN :: Asigning user role failed with error: #{parsed_response['message']}"
+      end
+    rescue
+      Rails.logger.error "DISCOURSE DISCORD PLUGIN :: Asigning user role failed while parsing the response"
+    end
   end
 
-  def self.kick_user(email, role)
+  def self.remove_role(guild_id, user_id, role_id)
+    discord_host = SiteSetting.discord_host
+    response = Faraday.delete do |req|
+      req.url "#{discord_host}/guilds/#{guild_id}/members/#{user_id}/roles/#{role_id}"
+    end
 
+    begin
+      parsed_response=JSON.parse response.body
+      if parsed_response["status"] == "error"
+        Rails.logger.error "DISCOURSE DISCORD PLUGIN :: Asigning user role failed with error: #{parsed_response['message']}"
+      end
+    rescue
+      Rails.logger.error "DISCOURSE DISCORD PLUGIN :: Asigning user role failed while parsing the response"
+    end
   end
 end
